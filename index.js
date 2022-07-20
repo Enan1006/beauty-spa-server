1888ec80494a50e1d59403d346f65a23f76121ec
Imdadul Haque Enan <ihenan1006@gmail.com>
2022-07-20 23:54:54 +0600

    find all data

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const cors = reqquire('cors');
+const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const app = express()
+const app = express();
 const port = process.env.PORT || 5000;
 
 
@@ -12,7 +12,23 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zqs7v.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db("beautySpa").collection("slots");
 
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
